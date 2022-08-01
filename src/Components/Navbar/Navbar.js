de3697ed42d968f1e227d93839619c7e6fa7cfb5
Darren Au <darren.au0@utexas.edu>
2022-07-31 23:31:31 -0500

    asdf

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import ScrollToTop from "./ScrollToTop";
 
 const NavBar = () => {
   return (
-    <div className="bg-[#adb5bd]/75 text-2xl sticky top-0 flex justify-center items-center flex-wrap font-bold">
+    <div className="bg-[#adb5bd]/75 text-2xl sticky top-0 z-40 flex justify-center items-center flex-wrap font-bold">
       <div className="flex max-w-[1400px] w-full justify-between items-center overflow-hidden ">
         <ScrollToTop />
         <div className="flex flex-row">
